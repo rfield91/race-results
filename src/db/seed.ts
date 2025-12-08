0ba041a84e4ba92ffd9be9596ad9d315ddf6a363
Ryan Field <navalhawkeye@gmail.com>
2025-12-08 15:50:49 -0500

    Added seed script and test orgs

---
+++ src/db/seed.ts
@@ -0,0 +1,17 @@
+import { db } from "@/db";
+import "dotenv/config";
+import { orgs } from "./schema";
+
+
+async function main() {
+    await db.delete(orgs);
+
+    const createOrgs = await db
+        .insert(orgs)
+        .values((await import("@/db/seed-data/orgs.json")).default)
+        .returning();
+
+    console.log(createOrgs);
+}
+
+main();
